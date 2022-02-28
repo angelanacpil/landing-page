f3faeaf561bff4a58f08ea9309934ec937b91ec1
Angela <96483922+angelanacpil@users.noreply.github.com>
2022-02-28 22:46:27 +0800

    WIP: Cover & Sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
 import "./styles.css";
-import LogoCard from "./components/LogoCard";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import NavBar from "./components/NavBar";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import CoverPic from "./components/malabon-city-hall.jpg";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+
+// FONTS
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export default function App() {
-  return <LogoCard></LogoCard>;
+  return (
+    <CssBaseline>
+      <Grid container spacing={8} direction="column">
+        {/* NAV */}
+        <Grid item>
+          <NavBar></NavBar>
+        </Grid>
+        {/* COVER */}
+        <Grid item>
+          <main>
+            <Box sx={{ p: { xs: 10, lg: 20 } }}>
+              <Typography
+                mb={5}
+                sx={{
+                  fontWeight: "bold",
+                  textAlign: { xs: "center", sm: "left" },
+                  fontSize: { xs: "1.4em", sm: "2em", md: "3em" },
+                  maxWidth: "20ch"
+                }}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </Typography>
+              <Button variant="outlined">REGISTER NOW</Button>
+            </Box>
+          </main>
+        </Grid>
+        {/* SECTIONS */}
+        <Grid item></Grid>
+      </Grid>
+    </CssBaseline>
+  );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,55 @@
+// IMPORTS
+import MenuIcon from "@mui/icons-material/Menu";
+import Stack from "@mui/material/Stack";
+import Tabs from "@mui/material/Tabs";
+import React from "react";
+import Tab from "@mui/material/Tab";
+import Logo from "./malabon-img.png";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+
+export default function NavBar() {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event: any, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Card sx={{ position: "fixed", right: 0, top: 0, left: 0, p: 1.5 }}>
+      <Stack
+        spacing={2}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Stack spacing={2} direction="row" alignItems="center">
+          <img src={Logo} alt="Logo" height="60" />
+          {/* LOGO */}
+          <Typography sx={{ fontWeight: "bold" }}>
+            Malabon Online Services
+          </Typography>
+          {/* ICON */}
+          <IconButton sx={{ display: { xs: "block", sm: "none" } }}>
+            <MenuIcon fontSize="large"></MenuIcon>
+          </IconButton>
+        </Stack>
+        {/* NAV LINKS */}
+        <Tabs
+          sx={{ display: { xs: "none", sm: "block" } }}
+          value={value}
+          onChange={handleChange}
+        >
+          <Tab value="one" label="HOME" />
+          <Tab value="two" label="BULLETIN" />
+          <Tab value="three" label="TERMS & CONDITIONS" />
+          <Button variant="outlined" href="#" sx={{ ml: 3 }}>
+            Sign Up
+          </Button>
+        </Tabs>
+      </Stack>
+    </Card>
+  );
+}
