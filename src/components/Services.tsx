6924366a349c9aec52c7e765fc8020e20a90ee74
Angela <96483922+angelanacpil@users.noreply.github.com>
2022-03-03 21:28:05 +0800

    ❗: Steps&Req | ✅Services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -8,6 +8,12 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import { Grid } from "@mui/material";
 
+const cardStyle = {
+  p: 2,
+  my: 2,
+  backgroundColor: "#e1e1e1"
+};
+
 export default function Services() {
   return (
     <Box
@@ -16,9 +22,9 @@ export default function Services() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        gap: "1rem",
+        gap: "1.5rem",
         textAlign: "center",
-        px: 2
+        px: 3
       }}
     >
       <Typography sx={{ fontWeight: "bold" }} variant="h4">
@@ -27,50 +33,59 @@ export default function Services() {
       <Typography sx={{ color: "gray" }}>
         Lorem ipsum dolor sit amet consectetur adipisicing.
       </Typography>
-      <Stack
+      <Grid
+        container
         direction={{ xs: "column", md: "row" }}
-        spacing={5}
-        justifyContent="space-around"
+        justifyContent="center"
         alignItems="center"
-        sx={{ maxWidth: "auto", maxHeight: "auto", px: 5 }}
+        spacing={4}
+        sx={{ maxWidth: "75rem" }}
       >
         {/* SERVICE 1 */}
-        <Card sx={{ p: 2, my: 4, backgroundColor: "#e1e1e1" }}>
-          <Stack justifyContent="center" alignItems="center" spacing={2}>
-            <Avatar sx={{ width: 60, height: 60 }}></Avatar>
-            <Typography variant="h5">Service #1</Typography>
-            <Typography sx={{ lineHeight: "2" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-              molestiae!
-            </Typography>
-            <Button>Learn More</Button>
-          </Stack>
-        </Card>
+        <Grid item xs={4}>
+          <Card sx={cardStyle}>
+            <Stack justifyContent="center" alignItems="center" spacing={2}>
+              <Avatar sx={{ width: 60, height: 60 }}></Avatar>
+              <Typography variant="h5">Service #1</Typography>
+              <Typography sx={{ lineHeight: "2" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis, molestiae!
+              </Typography>
+              <Button>Learn More</Button>
+            </Stack>
+          </Card>
+        </Grid>
 
-        <Card sx={{ p: 2, my: 4 }}>
-          <Stack justifyContent="center" alignItems="center" spacing={2}>
-            <Avatar sx={{ width: 60, height: 60 }}></Avatar>
-            <Typography variant="h5">Service #1</Typography>
-            <Typography sx={{ lineHeight: "2" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-              molestiae!
-            </Typography>
-            <Button>Learn More</Button>
-          </Stack>
-        </Card>
+        {/* SERVICE 2 */}
+        <Grid item xs={4}>
+          <Card sx={cardStyle}>
+            <Stack justifyContent="center" alignItems="center" spacing={2}>
+              <Avatar sx={{ width: 60, height: 60 }}></Avatar>
+              <Typography variant="h5">Service #2</Typography>
+              <Typography sx={{ lineHeight: "2" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis, molestiae!
+              </Typography>
+              <Button>Learn More</Button>
+            </Stack>
+          </Card>
+        </Grid>
 
-        <Card sx={{ p: 2, my: 4 }}>
-          <Stack justifyContent="center" alignItems="center" spacing={2}>
-            <Avatar sx={{ width: 60, height: 60 }}></Avatar>
-            <Typography variant="h5">Service #1</Typography>
-            <Typography sx={{ lineHeight: "2" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-              molestiae!
-            </Typography>
-            <Button>Learn More</Button>
-          </Stack>
-        </Card>
-      </Stack>
+        {/* SERVICE 3 */}
+        <Grid item xs={4}>
+          <Card sx={cardStyle}>
+            <Stack justifyContent="center" alignItems="center" spacing={2}>
+              <Avatar sx={{ width: 60, height: 60 }}></Avatar>
+              <Typography variant="h5">Service #3</Typography>
+              <Typography sx={{ lineHeight: "2" }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis, molestiae!
+              </Typography>
+              <Button>Learn More</Button>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
